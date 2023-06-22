4b9c7c9c5abedba131b5b71ed53b337e1beb2c24
sanskarsharma100 <Thebestone@123>
2023-06-23 01:14:04 +0530

    Error handling for duplicates values and token

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,24 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  //Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json web token is invalid, try again `;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT EXPIRE error
+  if (err.name === "JsonWebExpiredError") {
+    const message = `Json web token is Expired, Try again `;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
