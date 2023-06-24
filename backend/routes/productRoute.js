328715c908d693d9ffcbff7b67c50c6a1165c3d8
sanskarsharma100 <Thebestone@123>
2023-06-24 18:31:31 +0530

    added api routes for create,update,delete and get all reviews

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,6 +5,9 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require("../controllers/ProductController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
@@ -22,5 +25,12 @@ router
   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
 router.route("/product/:id").get(getProductDetails);
+
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
