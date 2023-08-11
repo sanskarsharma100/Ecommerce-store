b1ea574cb70c024cbd9e90dc690e199199e4b5e0
sanskarsharma100 <Thebestone@123>
2023-08-11 05:56:25 +0530

    increased upload limit in Backend

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,16 @@ const fileUpload = require("express-fileupload");
 const errorMiddleware = require("./middleware/error");
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 app.use(fileUpload());
 
 //Route Imports
