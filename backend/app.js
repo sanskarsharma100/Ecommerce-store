22513e3969155bcb2b856212106ecc2d7bf3d78a
sanskarsharma100 <Thebestone@123>
2023-09-03 00:52:43 +0530

    add redirects and change frontend url in cors

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const fileUpload = require("express-fileupload");
 
 const errorMiddleware = require("./middleware/error");
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "50mb" }));
