2f13a8c5b353053161a5c846b8bb15deeeff498b
sanskarsharma100 <Thebestone@123>
2023-09-23 01:33:32 +0530

    fixed issue with pagination buttons

--- frontend/src/components/Products/Pagination.tsx
+++ frontend/src/components/Products/Pagination.tsx
@@ -27,7 +27,7 @@ export const Pagination: FC<Props> = ({
           sort: queryPara.sort,
           page: String(ind + 1),
         })}`}
-        className={`inline-block border-r-2 border-primary-400 px-1.5 py-0.5 text-xs font-semibold text-primary-900 hover:bg-primary-100 hover:font-bold xs:text-sm ss:px-3 ss:py-1 ss:text-base ${
+        className={`inline-block border-r-2 border-primary-400 px-2.5 py-0.5 text-base font-semibold text-primary-900 hover:bg-primary-100 hover:font-bold xs:text-sm ss:px-3 ss:py-1 ${
           currentPage == ind + 1 && "bg-primary-400 hover:!bg-primary-400"
         }`}
       >
@@ -47,7 +47,7 @@ export const Pagination: FC<Props> = ({
           sort: queryPara.sort,
           page: String(queryPara.currentPage - 1),
         })}`}
-        className={`inline-block border-r-2 border-primary-400 px-1 py-0.5 text-xs font-medium text-primary-900 hover:bg-primary-100 xs:text-sm ss:px-2 ss:py-1 ss:text-base  ${
+        className={`inline-block border-r-2 border-primary-400 px-1 py-0.5 text-base font-medium text-primary-900 hover:bg-primary-100 xs:text-sm ss:px-2 ss:py-1  ${
           currentPage <= 1 && "hidden"
         }`}
       >
@@ -64,7 +64,7 @@ export const Pagination: FC<Props> = ({
           sort: queryPara.sort,
           page: String(queryPara.currentPage + 1),
         })}`}
-        className={`inline-block px-1 py-0.5 text-xs font-medium text-primary-900 hover:bg-primary-100 xs:text-sm ss:px-2 ss:py-1 ss:text-base ${
+        className={`inline-block px-1 py-0.5 text-base font-medium text-primary-900 hover:bg-primary-100 xs:text-sm ss:px-2 ss:py-1 ${
           currentPage == numOfPages && "hidden"
         }`}
       >
